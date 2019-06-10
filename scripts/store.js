a26a96d475b877c88e12bf3bf66e562b10d54b09
Adrienne Smith <agsmith046@gmail.com>
2019-06-10 11:09:43 -0500

    CRUD functions into store.js

--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-/* global cuid */
+/* global cuid, Item */
 
 const store = (function () {
   const foo = 'bar';
@@ -11,9 +11,58 @@ const store = (function () {
   ];
   let hideCheckedItems = false;
   let searchTerm = '';
+  
+  let findById = function(id) {
+    return this.items.find(function(item) {
+      if (item.id === id) {
+        return true;
+      }
+    });
+  };
+  
+  let addItem = function(name){
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(err) {
+      console.log('Cannot add item: ' + err.message);
+    }
+  };
+  
+  let findAndToggleChecked = function(id) {
+    let foundItem = this.findById(id);
+    if (foundItem.checked) {
+      foundItem.checked = false;
+    } else {
+      foundItem.checked = true;
+    }
+    return foundItem;
+  };
+
+  let findAndUpdateName = function(id, newName) {
+    try{
+      Item.validateName(newName);
+      let foundItem = this.findById(id);
+      foundItem.name = newName;
+      return foundItem;
+    }
+    catch(err){
+      console.log('Cannot update name: ' + err.message);
+    }
+  };
+
+  let findAndDelete = function(id) {
+    this.items = this.items.filter(item => item.id !== id);
+  };
+  
   return {
     items,
     hideCheckedItems,
-    searchTerm
+    searchTerm,
+    findById,
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete,
   };
 }());
